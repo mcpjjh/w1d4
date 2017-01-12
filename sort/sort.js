9313429913858907977d453509157f35d6bf1872
Young <mcpjjh@gmail.com>
2017-01-12 20:54:09 +0000

    sort names alphabetically and by older age if same name

---
+++ sort/sort.js
@@ -0,0 +1,25 @@
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+var result = students.sort(function(a, b) {
+  var nameA = a.name.toUpperCase();
+  var nameB = b.name.toUpperCase();
+  if (nameA < nameB) {
+    return -1;
+  }
+  if (nameA > nameB) {
+    return 1;
+  }
+  if (nameA = nameB) {
+    return b.age - a.age;
+  }
+
+  return 0;
+});
+
+
+console.log(result);
